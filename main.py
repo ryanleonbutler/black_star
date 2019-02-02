49e541037e85bd29f968476795b6c1d51664c6dd
Ryan <ryanleonbutler@gmail.com>
2019-02-02 16:11:01 +0200

    2019-02-02 16:11:01

--- main.py
+++ main.py
@@ -19,40 +19,58 @@ if __name__ == '__main__':
     # class Character:
     # TODO: Create Character class
     class Character:
-        NAME = ""
-
-    # class Inventory:
-    # TODO: Create Inventory class
+        NAME = ''
+        LEVEL = 0
+        HEALTH = 0
+        ARMOR = 0
+        DAMAGE = 0
+        HEAD = 'None'
+        CHEST = 'None'
+        WEAPON = 'None'
 
     # class Object:
     # TODO: Create Object class
     class Object:
-        NAME = 'Piece of paper'
+        NAME = ''
         ARMOR = 0
         DAMAGE = 0
-        WEAR = 0
-        USE = 'Read'
-        Note = 'Blahblahblah'
+        USE = ''
+        Note = ''
+
+    # class Inventory:
+    # TODO: Create Inventory class
+    class Inventory:
+        BAG = []
 
     # class Room:
     # TODO: Create Room class
     class Room:
-        NAME = 'Small Jail Cell'
-        GROUND = Object.NAME
+        NAME = ''
+        UP = ''
+        DOWN = ''
+        LEFT = ''
+        RIGHT = ''
+        OBJECT = []
 
     # class Enemy:
     # TODO: Create Enemy class
-
+    class Enemy:
+        NAME = ''
+        LEVEL = 1
+        HEALTH = 100
+        ARMOR = 1
+        DAMAGE = 1
     # Functions
     # ------------------------------------------------------------------------------------------------------------------
+
     def gprint(sentence):
-        print(Fore.GREEN + sentence)
+        print(Style.BRIGHT + Fore.GREEN + sentence)
 
     def wprint(sentence):
-        print(Fore.WHITE + sentence)
+        print(Style.BRIGHT + Fore.WHITE + sentence)
 
     def rprint(sentence):
-        print(Fore.RED + sentence)
+        print(Style.BRIGHT + Fore.RED + sentence)
 
     def yprint(sentence):
         print(Style.BRIGHT + Fore.YELLOW + sentence)
@@ -68,17 +86,23 @@ if __name__ == '__main__':
         wprint('Help Menu:')
         wprint('- type \'look\' to look around in this area')
         wprint('- type \'take\' to take object into inventory')
+        wprint('- type \'inventory\' to see what is in your bag, current gear on player and status')
         wprint('- type \'up\', \'down\', \'left\' and \'right\' to move around in this area')
 
     def character_create():
-        gprint('Enter your character\'s name:')
-        char_name = input(str())
-        char_level = 1
+        bprint('Enter your character\'s name:')
+        Character.NAME = input(str())
+        Character.LEVEL = 1
+        Character.HEALTH = 150
+        Character.DAMAGE = 5
+        Character.ARMOR = 1
+        Character.HEAD = 'Empty'
+        Character.CHEST = 'Tunic'
+        Character.WEAPON = 'Empty'
+        Inventory.BAG.append('Empty')
         clear()
-        bprint(f'Welcome young {char_name}, may the force be with you...')
+        bprint(f'Welcome young {Character.NAME}, may the force be with you...')
         time.sleep(1)
-        bprint(f'You are currently level: {char_level}')
-        time.sleep(2)
         clear()
         time.sleep(1)
 
@@ -97,94 +121,96 @@ if __name__ == '__main__':
 
     # Start of game loop
     while game:
-
         # Welcome note where games starts
-        yprint('Welcome to Black Star - TEXT'.center(width))
+        clear()
         time.sleep(1)
-        bprint('A Classic Text-Based Adventure Game!\n\n'.center(width))
-        wprint('MAIN MENU')
-        wprint('1. Play')
-        wprint('2. Options')
-        wprint('3. Quit')
+        bprint('Long ago,'.center(width))
+        bprint('in a star system very far away...\n'.center(width))
+        time.sleep(1)
+        yprint('Black Star'.center(width))
+        wprint('A Text-Based Adventure'.center(width))
+        time.sleep(1)
+        wprint('Developed by Ryan Butler'.center(width))
+        time.sleep(1)
+        clear()
+        yprint('Black Star'.center(width))
+        wprint('A Text-Based Adventure\n\n'.center(width))
+        wprint('MAIN MENU\n1. Play\n2. Quit')
 
         # Player menu option input
         menuOption = int(input())
-
         # Menu Item 1
         # Playing the game
         if menuOption == 1:
             clear()
             game = True
             playGame = True
-            gameChapter = 'Intro'
             while playGame:
                 # Chapter: Intro
-                time.sleep(1)
-                bprint('Long ago,'.center(width))
-                bprint('in a star system very far away...\n'.center(width))
-                time.sleep(1)
-                yprint('Black Star'.center(width))
-                wprint('A Text-Based Adventure\n'.center(width))
-                time.sleep(1)
-                wprint('Developed by Ryan Butler'.center(width))
-                time.sleep(1)
-                clear()
-
                 # Character Creation
                 character_create()
-
                 # Chapter: Intro
-                wprint('You: uhhhh...ahhhh...')
+                intro = True
+                Room.NAME = 'Jail Cell'
+                Room.UP = 'Door'
+                Room.DOWN = 'Window'
+                Room.LEFT = 'Wall'
+                Room.RIGHT = 'Wall'
+                Room.OBJECT = ['Key']
+                wprint(f'{Character.NAME}: uhhhh...ahhhh...')
                 time.sleep(1)
-                wprint('You: my head...what happened???')
-                bprint('Game: You are in a small room')
-                time.sleep(1)
-                playerInput = str(input())
-                if playerInput == 'q' or playerInput == 'quit' or playerInput == 'Quit':
-                    playGame = False    # End the game
-                elif playerInput == 'h' or playerInput == 'help' or playerInput == 'Help':
-                    player_help()
-                    playGame = True
-
-        # Menu Item 2
-        # Options menu
-        elif menuOption == 2:
-            game = True
-            options = True
-            while options:
-                wprint('Options: \n1. Difficulty \n2. Text Color \n3. Back to Main Menu')
-                optionInput = int(input())
-                if optionInput == 1:
-                    options = True
-                elif optionInput == 2:
-                    colorMenu = True
-                    # Display text - color menu
-                    while colorMenu:
-                        wprint('Text Colors: \n1. Purple \n2. Blue \n3. Green \n4. Default\n5. Back to Options')
-                        colorInput = int(input())
-                        if colorInput == 1:
-                            userText = Fore.MAGENTA
-                            colorMenu = False
-                        elif colorInput == 2:
-                            userText = Fore.BLUE
-                            colorMenu = False
-                        elif colorInput == 3:
-                            userText = Fore.GREEN
-                            colorMenu = False
-                        elif colorInput == 4:
-                            userText = Fore.WHITE
-                            colorMenu = False
+                wprint(f'{Character.NAME}: my head...what happened???')
+                bprint(f'You are in a {Room.NAME}')
+                time.sleep(2)
+                bprint('Hint: type \'h\' or \'help\' for controls')
+                while intro:
+                    playerInput = str(input())
+                    if playerInput == 'q' or playerInput == 'quit':
+                        intro = False
+                        playGame = False    # End the game
+                    elif playerInput == 'h' or playerInput == 'help':
+                        player_help()
+                    elif playerInput == 'll' or playerInput == 'look':
+                        bprint(f'You are in a {Room.NAME}')
+                        yprint(f'Up is {Room.UP}')
+                        yprint(f'Down is {Room.DOWN}')
+                        yprint(f'To your left is {Room.LEFT}')
+                        yprint(f'To your right is {Room.RIGHT}')
+                        gprint(f'On the ground is {Room.OBJECT[-1]}')
+                    elif playerInput == 'i' or playerInput == 'inventory':
+                        gprint(f'---INVENTORY---\nHead: {Character.HEAD}\tChest: {Character.CHEST}\n'
+                               f'Weapon: {Character.WEAPON}')
+                        gprint(f'Bag: {Inventory.BAG[-1]}')
+                        bprint(f'\n---STATUS---\nLevel: {Character.LEVEL}\tHealth: {Character.HEALTH}')
+                        rprint(f'\n---ATTRIBUTES---\nDamage: {Character.DAMAGE}\tArmor: {Character.ARMOR}')
+                    elif playerInput == 'l' or playerInput == 'left':
+                        yprint(f'You moved to {Room.LEFT}')
+                    elif playerInput == 'r' or playerInput == 'right':
+                        yprint(f'You moved to {Room.RIGHT}')
+                    elif playerInput == 'u' or playerInput == 'up':
+                        yprint(f'You moved to {Room.UP}')
+                        if 'Key' in Inventory.BAG:
+                            bprint('Unlock door with Key? (y/n)')
+                            playerInput = str(input())
+                            if playerInput == 'y':
+                                # Move to passage
+                                yprint('Moved to Passage')
+                                Inventory.BAG.pop()
+                            else:
+                                intro = True
                         else:
-                            colorMenu = True
-                elif optionInput == 3:
-                    clear()
-                    options = False
-                else:
-                    options = True
-
-        # Menu Item 3
+                            bprint('The Door is locked and needs a key')
+                    elif playerInput == 'd' or playerInput == 'down':
+                        yprint(f'You moved to {Room.DOWN}')
+                    elif playerInput == 't' or playerInput == 'take':
+                        gprint(f'You picked up {Room.OBJECT[-1]}')
+                        Inventory.BAG.append(Room.OBJECT[-1])
+                        Room.OBJECT.pop()
+                    else:
+                        intro = True
+        # Menu Item 2
         # Quit game option
-        elif menuOption == 3:
+        elif menuOption == 2:
             clear()
             game = False
         else:
